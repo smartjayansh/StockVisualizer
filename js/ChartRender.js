41671ddcfc067647dc9d13b116b5cfe3a7717509
Jayansh Goyal <jayanshgoyal@microsoft.com>
2023-06-24 14:55:05 +0530

    Add support to fetch data from api and render on chart

--- js/ChartRender.js
+++ js/ChartRender.js
@@ -1,47 +1,58 @@
-//import stockPriceData from './StockData';
-//import stockPriceData from './StockData';
-let sampleData = "{\"MetaData\":{\"1.Information\":\"DailyTimeSerieswithSplitsandDividendEvents\",\"2.Symbol\":\"SHOP\",\"3.LastRefreshed\":\"2023-06-22\",\"4.OutputSize\":\"Compact\",\"5.TimeZone\":\"US/Eastern\"},\"TimeSeries(Daily)\":{\"2023-06-22\":{\"1.open\":\"62.65\",\"2.high\":\"64.0599\",\"3.low\":\"62.05\",\"4.close\":\"63.48\",\"5.adjustedclose\":\"63.48\",\"6.volume\":\"11143057\",\"7.dividendamount\":\"0.0000\",\"8.splitcoefficient\":\"1.0\"},\"2023-06-21\":{\"1.open\":\"64.16\",\"2.high\":\"64.85\",\"3.low\":\"61.85\",\"4.close\":\"63.17\",\"5.adjustedclose\":\"63.17\",\"6.volume\":\"12508578\",\"7.dividendamount\":\"0.0000\",\"8.splitcoefficient\":\"1.0\"}}}";
-console.log(sampleData);
+import stockData from './StockData.js';
+// let sampleData = "{\"MetaData\":{\"1.Information\":\"DailyTimeSerieswithSplitsandDividendEvents\",\"2.Symbol\":\"SHOP\",\"3.LastRefreshed\":\"2023-06-22\",\"4.OutputSize\":\"Compact\",\"5.TimeZone\":\"US/Eastern\"},\"TimeSeries(Daily)\":{\"2023-06-22\":{\"1.open\":\"62.65\",\"2.high\":\"64.0599\",\"3.low\":\"62.05\",\"4.close\":\"63.48\",\"5.adjustedclose\":\"63.48\",\"6.volume\":\"11143057\",\"7.dividendamount\":\"0.0000\",\"8.splitcoefficient\":\"1.0\"},\"2023-06-21\":{\"1.open\":\"64.16\",\"2.high\":\"64.85\",\"3.low\":\"61.85\",\"4.close\":\"63.17\",\"5.adjustedclose\":\"63.17\",\"6.volume\":\"12508578\",\"7.dividendamount\":\"0.0000\",\"8.splitcoefficient\":\"1.0\"}}}";
+// sampleData = JSON.parse(sampleData);
+
+let stockPriceData;
+// Function to get stock price data
+async function getStockPriceData() {
+    // Call the fetchStockPrices method to get the stock price data
+    stockPriceData = await stockData.fetchStockPrices();
+}
+// Call the getStockPriceData function to get the stock price data
+getStockPriceData().then(() => {
+    const data = {
+        datasets: [{
+            data: [
+            ],
+        }]
+    };
+
+    const timeSeries = stockPriceData['Time Series (Daily)'];
+    for (const date in timeSeries) {
+        const dataPoint = timeSeries[date];
+        const dateTime = luxon.DateTime.fromFormat(date, 'yyyy-MM-dd');
+
+        const chartDataPoint = {
+            x: dateTime.valueOf(),
+            o: parseFloat(dataPoint['1. open']),
+            h: parseFloat(dataPoint['2. high']),
+            l: parseFloat(dataPoint['3. low']),
+            c: parseFloat(dataPoint['4. close'])
+        };
+
+        data.datasets[0].data.push(chartDataPoint);
+    }
+
+    // config 
+    const config = {
+        type: 'candlestick',
+        data,
+        options: {}
+    };
+
+    // render init block
+    const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+
+    // Instantly assign Chart.js version
+    const chartVersion = document.getElementById('chartVersion');
+    chartVersion.innerText = Chart.version;
+
+}).catch((error) => {
+    console.log('Error rendering chart:', error.message);
+});
 
 
-// setup 
-const startingDate = luxon.DateTime.fromRFC2822('04 Jan 2021 00:00:00 GMT');
-const date2 = luxon.DateTime.fromRFC2822('06 Jan 2021 00:00:00 GMT');
 
-const data = {
-    datasets: [{
-        data: [
-            {
-                x: startingDate.valueOf(),
-                o: 0.5,
-                h: 1,
-                l: 0.4,
-                c: 0.7,
-            },
-            {
-                x: date2.valueOf(),
-                o: 0.7,
-                h: 1.5,
-                l: 0.2,
-                c: 0.4,
-            }
-        ],
-    }]
-};
-
-// config 
-const config = {
-    type: 'candlestick',
-    data,
-    options: {}
-};
-
-// render init block
-const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-);
-
-// Instantly assign Chart.js version
-const chartVersion = document.getElementById('chartVersion');
-chartVersion.innerText = Chart.version;
